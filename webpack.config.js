1a9f1c6ebcbb847228bdee50169e820b004af9d7
Andrius Zidonis <andrius.zidonis@hostinger.com>
2019-11-13 22:54:42 +0200

    add agent 01

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
-const path = require('path');
-const scene = 'scene4';
+const path = require("path");
+const scene = "agent01";
 
 module.exports = {
   entry: `./${scene}/index.js`,
   // mode: 'production',
   output: {
-    filename: 'main.js',
+    filename: "main.js",
     path: path.resolve(__dirname, `./${scene}`)
   },
   devServer: {
     contentBase: `./${scene}`,
     disableHostCheck: true
-  },
+  }
 };
