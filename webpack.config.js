028b9b5cc9f3287258a3ef78f1bb7ae663138e76
Andrius Zidonis <andrius.zidonis@hostinger.com>
2020-02-08 16:41:10 +0200

    update khari main.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,21 @@ const scene = "scene-khari";
 
 module.exports = {
   entry: `./${scene}/index.js`,
-  // mode: 'production',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ["@babel/plugin-transform-template-literals"]
+          }
+        }
+      }
+    ]
+  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, `./${scene}`)
