570cf2400647939dead0660fe70897d652a3b029
Julius <julius@Burning-iMac.local>
2020-06-16 14:33:03 +0300

    removed old scenes

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,6 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    'scene-agent': './scene-agent/index.js',
-    'scene-chicago': './scene-chicago/index.js',
-    'scene-jemba': './scene-jemba/index.js',
-    'scene-khari': './scene-khari/index.js',
     'scene-opium': './scene-opium/index.js',
     'scene-video': './scene-video/index.js',
     'scene-ioannina': './scene-ioannina/index.js',
