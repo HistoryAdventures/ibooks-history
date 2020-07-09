2d31fcd91954b74176190bed1014fa70269d7d6b
Andrius Zidonis <andrius.zidonis@hostinger.com>
2020-07-10 00:29:49 +0300

    increase animation length

--- scene-ioannina/index.js
+++ scene-ioannina/index.js
@@ -124,13 +124,13 @@ function init() {
         var group = new THREE.Group();
 
         var geometry = new THREE.RingGeometry( 4.8, 5, 32 );
-        var material = new THREE.MeshBasicMaterial( { color: 0xffffff, side: THREE.DoubleSide } );
+        var material = new THREE.MeshBasicMaterial( { color: 0xffffff, side: THREE.DoubleSide, opacity: 1, transparent: true, } );
         var mesh = new THREE.Mesh( geometry, material );
         group.add( mesh );
         mesh.scale.set(0.02, 0.02, 0);
 
         var geometry2 = new THREE.RingGeometry( 4, 4.5, 32 );
-        var material2 = new THREE.MeshBasicMaterial( { color: 0xffffff, side: THREE.DoubleSide } );
+        var material2 = new THREE.MeshBasicMaterial( { color: 0xffffff, side: THREE.DoubleSide, opacity: 1, transparent: true, } );
         var mesh2 = new THREE.Mesh( geometry2, material2 );
         group.add( mesh2 );
         mesh2.scale.set(0.035, 0.035, 0);
@@ -142,23 +142,23 @@ function init() {
         group.add( circle );
 
         new TWEEN.Tween(mesh.scale)
-            .delay(400)
+            .delay(600)
             .to({
                 x: 0.15,
                 y: 0.15,
                 z: 0
-            }, 1100)
+            }, 3000)
             .easing(TWEEN.Easing.Linear.None)
             .repeat(Infinity)
             .start();
 
         new TWEEN.Tween(mesh2.scale)
-            .delay(400)
+            .delay(600)
             .to({
                 x: 0.175,
                 y: 0.175,
                 z: 0
-            }, 1100)
+            }, 3000)
             .easing(TWEEN.Easing.Quadratic.In)
             .repeat(Infinity)
             .start();
